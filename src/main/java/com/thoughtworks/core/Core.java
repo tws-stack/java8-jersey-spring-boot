c56d39e908ffa011b7606b2735730d38a81b3eee
Liang Hong <lianghongbuaa@gmail.com>
2016-12-14 00:13:20 +0800

    Add Buzz Impletation.

--- src/main/java/com/thoughtworks/core/Core.java
+++ src/main/java/com/thoughtworks/core/Core.java
@@ -7,6 +7,8 @@ public class Core {
     public String convertToNumberDescription(int number) {
         if (number % 3 == 0) {
             return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
         }
         return "1";
     }
