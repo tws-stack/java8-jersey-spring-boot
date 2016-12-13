93239586f5a363ef447984a456d606d67a244a53
Liang Hong <lianghongbuaa@gmail.com>
2016-12-14 00:38:20 +0800

    Add convert Implatation.

---
+++ src/main/java/com/thoughtworks/core/Convert.java
@@ -0,0 +1,16 @@
+package com.thoughtworks.core;
+
+/**
+ * Created by hliang on 14/12/2016.
+ */
+public class Convert {
+    private Core core;
+
+    public Convert(Core p0) {
+        core = p0;
+    }
+
+    public String[] convertToNumberDescriptions() {
+        return new String[0];
+    }
+}
