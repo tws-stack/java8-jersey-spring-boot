fc6e0f429a4529124f2eae118fad3fcdccaad658
Liang Hong <lianghongbuaa@gmail.com>
2016-12-14 00:02:19 +0800

    Add first implatation.

---
+++ src/main/java/com/thoughtworks/core/Core.java
@@ -0,0 +1,10 @@
+package com.thoughtworks.core;
+
+/**
+ * Created by hliang on 13/12/2016.
+ */
+public class Core {
+    public String convertToNumberDescription(int number) {
+        return "1";
+    }
+}
