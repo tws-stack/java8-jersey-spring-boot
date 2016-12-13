22b9f2298e4f424b5ee8e0273df2ce78487139e3
Liang Hong <lianghongbuaa@gmail.com>
2016-12-13 23:55:32 +0800

    Add convert test;

---
+++ src/test/java/com/thoughtworks/core/ConvertTest.java
@@ -0,0 +1,19 @@
+package com.thoughtworks.core;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Created by hliang on 13/12/2016.
+ */
+public class ConvertTest {
+    @Test
+    public void should_return_1_given_1 () {
+        //given
+        int number = 1;
+        //when
+        String actualValue = new Core().convertToNumberDescription(number);
+        //then
+        assertThat(actualValue).isEqualTo("1");
+    }
+}
